362380a0e0e682ffbabaf0ccdbf61f15d5949237
sssbatcha <sssbatcha@users.noreply.github.com>
2026-08-12 15:20:30 +0530

    Add bubble sort and selection sort examples

---
+++ 2.bubble sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(arr):
+
+    n = len(arr)
+    for i in range(n):          
+        for j in range(0, n-i-1): 
+                if arr[j] > arr[j+1] :
+                    arr[j], arr[j+1] = arr[j+1], arr[j] 
+    return arr
+
+
+
+
+
+
+arr = [2,6,7,4,3,1]
+print("Before Sorting:",arr)
+print("After Sorting:",bubble_sort(arr))
---
+++ selecion_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+
+arr = [10,5,4,20,35,40]
+print("Before Sorting:",arr)
+print("After Sorting:",selection_sort(arr))
