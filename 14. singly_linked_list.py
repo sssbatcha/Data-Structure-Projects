3978fc971aaee08b7b214fad95cc2e42540185cc
sssbatcha <sssbatcha@users.noreply.github.com>
2026-08-17 11:20:34 +0530

    SinglyLinkedList.py

---
+++ 14. singly_linked_list.py
@@ -0,0 +1,26 @@
+print("Singly Linked List:")
+class SinglyLinkedListNodes:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+def insert(head):
+    new_node = SinglyLinkedListNodes(4)
+    if head is None:
+        head = new_node
+    else:
+        current = head
+        while current.next is not None:
+            current = current.next
+        current.next = new_node
+    return head
+
+def Traversal(head):
+    current = head
+    while current is not None:
+        print(current.data, end=" → ")
+        current = current.next
+    print("None")
+
+node1 = SinglyLinkedListNodes(10)
+node2 = SinglyLinkedListNodes(20)
+node3 = SinglyLinkedListNodes(30)
